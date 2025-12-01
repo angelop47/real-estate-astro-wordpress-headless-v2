87464b6784d6298734a31f41ab6c1e541765c250
angelop47 <pescettoangelo@gmail.com>
2025-12-01 07:02:52 -0300

    feat: Implement Vercel adapter and switch property detail page to server-side rendering.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: vercel(),
+  output: 'server',
 });
